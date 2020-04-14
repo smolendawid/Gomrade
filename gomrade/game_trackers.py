561932b9d74776ff8ebeddb5b8613011e727986e
Dawid Smoleń <smolendawid@gmail.com>
2020-04-14 19:04:04 +0200

    planning the work

--- gomrade/game_trackers.py
+++ gomrade/game_trackers.py
@@ -2,12 +2,64 @@ from sgfmill import sgf, ascii_boards, boards
 
 from gomrade.state_utils import project_stones_state
 
+"""
+What can happen:
+
+# Casual game:
+
+1. Color plays normally:
+- One extra stone for color and the same num of the other color
+
+2. Color plays and kills:
+- One extra stone for color and at least one less of the other color
+
+3. Undo to some move of a player
+- Check previous states and if it was, generate
+
+5. Undo to some move of AI
+- Check previous states and if it was, wait
+
+5. Undo to other move of a player
+- Check previous states and whether they differ with just one stone of player color
+
+6. Undo to other move of which is a kill
+- Pff
+
+# Introductions:
+
+1. Game state is introduced without sgf
+
+2. Game state is loaded from sgf
+
+3. Game state is strongly changed but some history is preserved
+
+
+# Errors:
+
+1. Random stone is switched to opposite
+- Just accept, fill with pass
+
+2. Stone or stones disappear
+
+3. A few stones are introduced
+
+
+
+1. Easy-peasy
+2. Need to check if it was a kill or a mistake. How verify the kill?
+- maybe checking the liberties will be necessary?
+3. 
+
+
+"""
+
 
 class GameTracker:
     def __init__(self):
         self.game = None
         self.board_size = None
         self.komi = None
+        self.state_history = []
 
     def many_stones_added(self):
         pass
@@ -49,10 +101,7 @@ class GameTracker:
     def vanilla_parse(self, stones_state):
         """Temporary method """
         # tmp = ''
-
         self.create_empty(self.board_size, self.komi)
-
-        stones_state = project_stones_state(stones_state, flip=True, rotate=False)
 
         for row in range(self.board_size):
             for col in range(self.board_size):
@@ -64,6 +113,9 @@ class GameTracker:
                 node.set_move(c.lower(), (row, col))
 
     def replay_position(self, stones_state):
+
+        stones_state = project_stones_state(stones_state, flip=True, rotate=False)
+        self.state_history.append(stones_state)
 
         self.vanilla_parse(stones_state)
         # node = self.game.extend_main_sequence()
